754f0ff89c9bb99b41b0cf53d3f12c8a0b1b8244
Richard Stewing <richard.stewing@udo.edu>
2020-09-11 16:27:17 +0200

    array function

--- ArrayFunc/program.adb
+++ ArrayFunc/program.adb
@@ -21,6 +21,7 @@ procedure Program is
    
    Result : Vector := Ongoing_Sum(Example);
    Result2 : Vector := Ongoing_Sum(Result);
+   
 begin
    Put("Example:");
    for I in Example'Range loop
@@ -41,6 +42,33 @@ begin
      Put(Result2(I));
    end loop;
    New_Line;
+   
+   declare
+      pragma Assertion_Policy(Check);
+      function Add(A, B : Vector) return Vector 
+	with Pre  => A'First = B'First and then A'Last = B'Last is
+	 C : Vector := (A'First .. A'Last => 0.0);
+      begin
+	 for I in A'Range loop
+	    C(I) := A(I) + B(I);
+	 end loop;
+	 return C;
+      end Add;
+   
+      Example2 : Vector := (1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0);
+   
+      Result3 : Vector := Add(Example, Example2);
+      --Result4 : Vector := Add(Result, Example); -- crashes before function is run
+   
+   begin
+   
+      Put("Result3:");
+      for I in Example2'Range loop
+	 Put(" ");
+	 Put(Result3(I));
+      end loop;
+      New_Line;
+   end;
 end Program;
   
    
