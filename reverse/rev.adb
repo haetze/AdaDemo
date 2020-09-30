947b1a245758163b33863f4d722f480ba3450296
Richard Stewing <richard.stewing@udo.edu>
2020-09-30 10:48:15 +0200

    with proof

--- reverse/rev.adb
+++ reverse/rev.adb
@@ -1,30 +1,26 @@
 package body Rev is
    function Reversed(A : in Arr; B : in Arr) return Boolean is
-      AF : Positive := A'First;
-      AL : Positive := A'Last;
-      BF : Positive := B'First;
-      BL : Positive := B'Last;
-   begin
-      if A'Length = B'Length and A'Length <= 1 then
-	 return True;
-      end if;
-      return AF < AL 
-	and then BF < BL 
-	and then (A'Length = B'Length) 
-	and then A(AF) = B(BL) 
-	and then Reversed(A(AF + 1 .. AL), B(BF .. BL - 1));
-   end Reversed;
+      (for all I in 0..A'Length - 1 => A(A'First + I) = B(B'Last - I));
    
    function Reverser(A : in Arr) return Arr is
       B : Arr := A;
       F : Positive := A'First;
       L : Positive := A'Last;
    begin
-      if A'Length <= 1 then 
-	 return B;
-      end if;
-      B(F + 1 .. L) := Reverser(A(F..L-1));
-      B(F) := A(L);
+      pragma Assert(B'First = A'First);
+      pragma Assert(B'Last = A'Last);
+      for I in 0..A'Length - 1 loop
+	 -- Proof for index access
+	 pragma Loop_Invariant(F + I <= L);
+	 pragma Loop_Invariant(L - I >= F);
+	 -- Previous swaps
+	 pragma Loop_Invariant(if I >= 1 then A(F + I - 1) = B(L - (I - 1))); 
+	 pragma Loop_Invariant(if I >= 2 then A(F + I - 2) = B(L - (I - 2))); 
+	 pragma Loop_Invariant(for all J in 1..I => A(F + I - J) = B(L - (I - J))); 
+	 
+	 B(L - I) := A(F + I);
+      end loop;
+      pragma Assert(for all J in 0..A'Length - 1 => A(F + J) = B(L - J));
       return B;
    end Reverser;
    
--- reverse/rev.ads
+++ reverse/rev.ads
@@ -9,14 +9,16 @@ package Rev is
      Pre => A'First in Positive 
      and A'Last in Positive 
      and B'Last in Positive
-     and B'First in Positive;
+     and B'First in Positive
+     and A'Length = B'Length;
    
    function Reverser(A : in Arr) return Arr
      with 
      Pre => A'First in Positive 
      and A'Last in Positive,
      Post => Reverser'Result'First = A'First
-     and Reverser'Result'Last = A'Last;
+     and Reverser'Result'Last = A'Last
+     and Reversed(A, Reverser'Result);
    
 private 
    
